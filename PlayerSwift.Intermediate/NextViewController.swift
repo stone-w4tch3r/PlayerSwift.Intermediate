0a8900dbe3cb10a3b4ab0671cdb992b157d7b397
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2023-06-29 20:24:12 +0500

    everything replaced with scrollview

--- PlayerSwift.Intermediate/NextViewController.swift
+++ PlayerSwift.Intermediate/NextViewController.swift
@@ -1,42 +1,39 @@
-//
-// Created by me on 2023/04/14.
-//
-
 import UIKit
 
-class NextViewController : UIViewController {
-    
-        let label = UILabel()
-        let button = UIButton(type: .roundedRect)
+class NextViewController: UIViewController {
+
+    let scrollView = UIScrollView()
+    let longTextView = UITextView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "NextVc"
+
+        setupSubviews()
+    }
+
+    private func setupSubviews() {
+        longTextView.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl. Donec euismod, nisl eget aliquam luctus, nunc nisl aliquet nunc, quis aliquam nisl nunc quis nisl."
+        longTextView.isScrollEnabled = false
+        longTextView.backgroundColor = .yellow
+        longTextView.font = UIFont.systemFont(ofSize: 30)
+        scrollView.backgroundColor = .green
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(longTextView)
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        longTextView.translatesAutoresizingMaskIntoConstraints = false
+
+        scrollView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 20).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            title = "NextVc"
-            
-            setupSubviews()
-        }
-        
-        private func setupSubviews() {
-            label.text = "here we are"
-            button.setTitle("go back", for: .normal)
-            button.addTarget(self, action: #selector(onButtonClicked), for: .touchUpInside)
-            
-            view.addSubview(label)
-            view.addSubview(button)
-            
-            label.translatesAutoresizingMaskIntoConstraints = false
-            button.translatesAutoresizingMaskIntoConstraints = false
-            
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20).isActive = true
-        }
-        
-        @objc func onButtonClicked(_ sender: UIButton){
-            if sender == button {
-                navigationController?.popViewController(animated: true)
-            }
-        }
+        longTextView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        longTextView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor).isActive = true
+        longTextView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        longTextView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+    }
 }
